087efc2e96f537a799d1a33899e3fcea16936800
Leon Tenório <leon_lts@hotmail.com>
2021-11-02 13:06:19 -0300

    refact: refatoração dos dados de cursos, projetos e matérias para os testes

--- src/services/repositories/Projeto.test.ts
+++ src/services/repositories/Projeto.test.ts
@@ -6,15 +6,144 @@ import { v4 as uuid } from 'uuid';
 import _ from 'lodash';
 import { ModelReadFactory } from './Model';
 
-test('Criação correta dos dados para armazenar a matéria', () => {
-  const materia: Materia = {
-    id: uuid(),
-    idPerfilProfessor: undefined,
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    idCurso: uuid(),
-  };
+const materia: Materia = {
+  id: uuid(),
+  idPerfilProfessor: undefined,
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  idCurso: uuid(),
+};
 
+const cursoVazio: Curso = {
+  id: uuid(),
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  inicioCurso: new Date(),
+  fimCurso: new Date(),
+  atualizadoEm: new Date(),
+  turma: [],
+  materias: [],
+  idProjeto: uuid(),
+};
+
+const idCurso: string = uuid();
+
+const materiaCurso: Materia = {
+  id: uuid(),
+  idPerfilProfessor: undefined,
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  idCurso: idCurso,
+};
+
+const cursoComMaterias: Curso = {
+  id: uuid(),
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  inicioCurso: new Date(),
+  fimCurso: new Date(),
+  atualizadoEm: new Date(),
+  turma: [uuid(), uuid()],
+  materias: [materiaCurso, materiaCurso],
+  idProjeto: uuid(),
+};
+
+const projetoNaoAprovado: Projeto = {
+  id: uuid(),
+  nome: 'Projeto Test',
+  descricao: 'Teste',
+  email: 'projeto@test.com',
+  telefone: 12999999999,
+  requisicaoEntradaEm: new Date(),
+  endereco: {
+    rua: 'Rua Teste',
+    numero: 0,
+    bairro: 'Bairro Teste',
+    cidade: 'Cidade Teste',
+    estado: 'SP',
+    cep: 12332000,
+    localizacao: {
+      lat: 0,
+      lng: 0,
+    },
+  },
+  aprovado: false,
+};
+
+const projetoAprovadoVazio: Projeto = {
+  id: uuid(),
+  nome: 'Projeto Test',
+  descricao: 'Teste',
+  email: 'projeto@test.com',
+  telefone: 12999999999,
+  requisicaoEntradaEm: new Date(),
+  endereco: {
+    rua: 'Rua Teste',
+    numero: 0,
+    bairro: 'Bairro Teste',
+    cidade: 'Cidade Teste',
+    estado: 'SP',
+    cep: 12332000,
+    localizacao: {
+      lat: 0,
+      lng: 0,
+    },
+  },
+  aprovado: true,
+  idPerfilResponsavel: uuid(),
+  entradaEm: new Date(),
+  cursos: [],
+};
+
+const idProjeto = uuid();
+const idCursoProjeto: string = uuid();
+
+const materiaCursoProjeto: Materia = {
+  id: uuid(),
+  idPerfilProfessor: undefined,
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  idCurso: idCursoProjeto,
+};
+
+const cursoProjeto: Curso = {
+  id: uuid(),
+  nome: 'Curso Test',
+  descricao: 'Teste',
+  inicioCurso: new Date(),
+  fimCurso: new Date(),
+  atualizadoEm: new Date(),
+  turma: [uuid(), uuid()],
+  materias: [materiaCursoProjeto, materiaCursoProjeto],
+  idProjeto: idProjeto,
+};
+
+const projetoAprovadoNaoVazio: Projeto = {
+  id: idProjeto,
+  nome: 'Projeto Test',
+  descricao: 'Teste',
+  email: 'projeto@test.com',
+  telefone: 12999999999,
+  requisicaoEntradaEm: new Date(),
+  endereco: {
+    rua: 'Rua Teste',
+    numero: 0,
+    bairro: 'Bairro Teste',
+    cidade: 'Cidade Teste',
+    estado: 'SP',
+    cep: 12332000,
+    localizacao: {
+      lat: 0,
+      lng: 0,
+    },
+  },
+  aprovado: true,
+  idPerfilResponsavel: uuid(),
+  entradaEm: new Date(),
+  cursos: [cursoProjeto, cursoProjeto],
+};
+
+test('Criação correta dos dados para armazenar a matéria', () => {
   const factory = new MateriaClass(materia).saveFactory();
   expect(factory).toStrictEqual([
     {
@@ -25,18 +154,6 @@ test('Criação correta dos dados para armazenar a matéria', () => {
 });
 
 test('Criação correta dos dados para armazenar um curso', () => {
-  const cursoVazio: Curso = {
-    id: uuid(),
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    inicioCurso: new Date(),
-    fimCurso: new Date(),
-    atualizadoEm: new Date(),
-    turma: [],
-    materias: [],
-    idProjeto: uuid(),
-  };
-
   const factoryCursoVazio = new CursoClass(cursoVazio).saveFactory();
   const dataCursoVazio: any = _.cloneDeep(cursoVazio);
   delete dataCursoVazio.materias;
@@ -44,28 +161,6 @@ test('Criação correta dos dados para armazenar um curso', () => {
   expect(factoryCursoVazio).toStrictEqual([
     { collection: 'Curso', data: dataCursoVazio },
   ]);
-
-  const idCurso: string = uuid();
-
-  const materia: Materia = {
-    id: uuid(),
-    idPerfilProfessor: undefined,
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    idCurso: idCurso,
-  };
-
-  const cursoComMaterias: Curso = {
-    id: uuid(),
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    inicioCurso: new Date(),
-    fimCurso: new Date(),
-    atualizadoEm: new Date(),
-    turma: [uuid(), uuid()],
-    materias: [materia, materia],
-    idProjeto: uuid(),
-  };
 
   const factoryCursoComMaterias = new CursoClass(
     cursoComMaterias
@@ -75,114 +170,22 @@ test('Criação correta dos dados para armazenar um curso', () => {
 
   expect(factoryCursoComMaterias).toStrictEqual([
     { collection: 'Curso', data: dataCursoComMaterias },
-    { collection: 'Materia', data: materia },
-    { collection: 'Materia', data: materia },
+    { collection: 'Materia', data: materiaCurso },
+    { collection: 'Materia', data: materiaCurso },
   ]);
 });
 
 test('Criação correta dos dados para armazenar um projeto', () => {
-  const projetoNaoAprovado: Projeto = {
-    id: uuid(),
-    nome: 'Projeto Test',
-    descricao: 'Teste',
-    email: 'projeto@test.com',
-    telefone: 12999999999,
-    requisicaoEntradaEm: new Date(),
-    endereco: {
-      rua: 'Rua Teste',
-      numero: 0,
-      bairro: 'Bairro Teste',
-      cidade: 'Cidade Teste',
-      estado: 'SP',
-      cep: 12332000,
-      localizacao: {
-        lat: 0,
-        lng: 0,
-      },
-    },
-    aprovado: false,
-  };
   expect(new ProjetoClass(projetoNaoAprovado).saveFactory()).toStrictEqual([
     { collection: 'Projeto', data: projetoNaoAprovado },
   ]);
 
-  const projetoAprovadoVazio: Projeto = {
-    id: uuid(),
-    nome: 'Projeto Test',
-    descricao: 'Teste',
-    email: 'projeto@test.com',
-    telefone: 12999999999,
-    requisicaoEntradaEm: new Date(),
-    endereco: {
-      rua: 'Rua Teste',
-      numero: 0,
-      bairro: 'Bairro Teste',
-      cidade: 'Cidade Teste',
-      estado: 'SP',
-      cep: 12332000,
-      localizacao: {
-        lat: 0,
-        lng: 0,
-      },
-    },
-    aprovado: true,
-    idPerfilResponsavel: uuid(),
-    entradaEm: new Date(),
-    cursos: [],
-  };
   const dataProjetoAprovadoVazio: any = _.cloneDeep(projetoAprovadoVazio);
   delete dataProjetoAprovadoVazio.cursos;
   expect(new ProjetoClass(projetoAprovadoVazio).saveFactory()).toStrictEqual([
     { collection: 'Projeto', data: dataProjetoAprovadoVazio },
   ]);
 
-  const idProjeto = uuid();
-  const idCurso: string = uuid();
-
-  const materia: Materia = {
-    id: uuid(),
-    idPerfilProfessor: undefined,
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    idCurso: idCurso,
-  };
-
-  const curso: Curso = {
-    id: uuid(),
-    nome: 'Curso Test',
-    descricao: 'Teste',
-    inicioCurso: new Date(),
-    fimCurso: new Date(),
-    atualizadoEm: new Date(),
-    turma: [uuid(), uuid()],
-    materias: [materia, materia],
-    idProjeto: idProjeto,
-  };
-
-  const projetoAprovadoNaoVazio: Projeto = {
-    id: idProjeto,
-    nome: 'Projeto Test',
-    descricao: 'Teste',
-    email: 'projeto@test.com',
-    telefone: 12999999999,
-    requisicaoEntradaEm: new Date(),
-    endereco: {
-      rua: 'Rua Teste',
-      numero: 0,
-      bairro: 'Bairro Teste',
-      cidade: 'Cidade Teste',
-      estado: 'SP',
-      cep: 12332000,
-      localizacao: {
-        lat: 0,
-        lng: 0,
-      },
-    },
-    aprovado: true,
-    idPerfilResponsavel: uuid(),
-    entradaEm: new Date(),
-    cursos: [curso, curso],
-  };
   const factoryProjetoNaoVazio = new ProjetoClass(
     projetoAprovadoNaoVazio
   ).saveFactory();
