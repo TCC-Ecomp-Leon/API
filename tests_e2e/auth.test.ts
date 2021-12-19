69817796a13ab2a80cfaa549f5d154646992d019
LeonLTS <leon_lts@hotmail.com>
2021-12-19 20:13:05 -0300

    feat: testes de entrada com usuário e senha

--- tests_e2e/auth.test.ts
+++ tests_e2e/auth.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import app from '../src/app';
 import { backtrackObject } from './utils';
+import { Perfil, RegraPerfil } from '../src/models';
+import assets from '../src/assets/images';
 
 const endpoint = '/auth/sign/';
 
@@ -20,6 +22,11 @@ const camposObrigatorios: {
 
 const camposOpcionais: { codigoDeEntrada: string } = {
   codigoDeEntrada: 'XXXXX',
+};
+
+const camposObrigatoriosLogin: { email: string; password: string } = {
+  email: camposObrigatorios.email,
+  password: camposObrigatorios.password,
 };
 
 test('Passagem de parâmetros errados para o endpoint de registro', async () => {
@@ -48,4 +55,34 @@ test('Tentativa de registrar um usuário novamente', async () => {
   const result = await request(app).post(endpoint).send(camposObrigatorios);
 
   expect(result.statusCode).toBeGreaterThanOrEqual(400);
+});
+
+test('Passagem de parâmetros errados para o endpoint de login', async () => {
+  const opcoesComErros = backtrackObject(camposObrigatoriosLogin, true, false);
+
+  for (let i = 0; i < opcoesComErros.length; i++) {
+    const result = await request(app).put(endpoint).send(opcoesComErros[i]);
+
+    expect(result.statusCode).toBe(400);
+  }
+});
+
+test('Tentativa de entrar com o usuário e senha registrados', async () => {
+  const result = await request(app).put(endpoint).send(camposObrigatoriosLogin);
+
+  expect(result.statusCode).toBe(200);
+
+  const perfil = result.body['profile'] as Perfil;
+
+  expect(perfil.email).toStrictEqual(camposObrigatorios.email);
+  expect(perfil.fotoPerfil).toStrictEqual(assets.imgPerfil);
+  expect(perfil.nome).toStrictEqual(camposObrigatorios.nome);
+  expect(perfil.telefone).toStrictEqual(camposObrigatorios.telefone);
+  expect(perfil.regra).toStrictEqual(RegraPerfil.Geral);
+
+  if (perfil.regra !== RegraPerfil.Geral) throw Error('');
+
+  expect(perfil.associacoes.aluno.alunoParceiro).toStrictEqual(false);
+  expect(perfil.associacoes.professor.professor).toStrictEqual(false);
+  expect(perfil.universitario.universitario).toStrictEqual(false);
 });
