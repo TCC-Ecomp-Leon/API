b1f55949b783642974bd80eb64ac787af1eed396
LeonLTS <leon_lts@hotmail.com>
2022-01-02 23:02:56 -0300

    wip

--- src/handlers/respostaAtividade/listarRespostas.ts
+++ src/handlers/respostaAtividade/listarRespostas.ts
@@ -1,4 +1,11 @@
+import {
+  DatabaseService,
+  withDatabaseTransaction,
+} from '../../config/database';
 import { Perfil, RegraPerfil, RespostaAtividade } from '../../models';
+import RepositorioProjeto from '../../services/repositories/RepositorioProjeto';
+import RepositorioResposta from '../../services/repositories/RepositorioResposta';
+import { DatabaseResult } from '../../structure/databaseResult';
 import Handler from '../../structure/handler';
 import {
   getCurrentProfile,
@@ -8,7 +15,7 @@ import {
 export const listarRespostasHandler = new Handler(
   async (
     context
-  ): Promise<NavigationResult<{ respostas: RespostaAtividade }>> => {
+  ): Promise<NavigationResult<{ respostas: RespostaAtividade[] }>> => {
     const userProfile = getCurrentProfile<Perfil>(context);
 
     const queryParamProjeto = context.query['projeto'];
@@ -35,5 +42,39 @@ export const listarRespostasHandler = new Handler(
         },
       };
     }
+
+    const service: DatabaseService<
+      NavigationResult<{ respostas: RespostaAtividade[] }>
+    > = async (db, session) => {
+      let result: DatabaseResult<RespostaAtividade[]>;
+      if (idAtividade !== undefined) {
+        result = await RepositorioResposta.lerRespostasEspecificas(
+          'idAtividade',
+          idAtividade,
+          db,
+          session
+        );
+      } else if (idProjeto !== undefined) {
+        result = await RepositorioResposta.lerRespostasEspecificas(
+          'idProjeto',
+          idAtividade,
+          db,
+          session
+        );
+      } else {
+        result = await RepositorioResposta.lerRespostas(db, session);
+      }
+
+      if (!result.success) throw result.error;
+
+      return {
+        status: 200,
+        body: {
+          respostas: result.data,
+        },
+      };
+    };
+
+    return await withDatabaseTransaction(service);
   }
 );
