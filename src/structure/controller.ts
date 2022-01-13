315dcc1c1a511766ea6e2e89339bebaec63b6b55
LeonLTS <leon_lts@hotmail.com>
2022-01-12 22:35:52 -0300

    feat: debug na entrada e saída do servidor

--- src/structure/controller.ts
+++ src/structure/controller.ts
@@ -5,9 +5,13 @@ import Navigation from './navigation';
 export default class Controller {
   async runNavigation(navigation: Navigation, req: Request, res: Response) {
     const context: Context = new Context(req);
+    console.log(context.method);
+    console.log(context.url);
+    console.log(context.body);
 
     const handlersResponse = await navigation.navigate(context);
     if (handlersResponse.success) {
+      console.log(handlersResponse.body);
       res.status(handlersResponse.status).send(handlersResponse.body);
     } else {
       console.warn('ERROR');
