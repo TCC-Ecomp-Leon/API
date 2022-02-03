15f543d61f0557d5c15260fb082c98f250c0e830
LeonLTS <leon_lts@hotmail.com>
2022-02-03 16:52:18 -0300

    fix: single test script

--- single_test.sh
+++ single_test.sh
@@ -1,3 +1,5 @@
-ENV='TEST' yarn ts-node-dev tests_e2e/test_data.ts ;
-ENV='TEST' yarn ts-node src/mocks/index.ts; 
-ENV='TEST' yarn jest --findRelatedTests $1
+ENV='TEST' ts-node src/env.ts; 
+ENV='TEST' ts-node tests_e2e/test_data.ts; 
+ENV='TEST' ts-node src/mocks/index.ts;
+
+ENV='TEST' jest --findRelatedTests $1
